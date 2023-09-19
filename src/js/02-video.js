222c6c5f77d5d4861e587972d3b18c15102fb036
TeodorMichalBaranski <126625089+TeodorMichalBaranski@users.noreply.github.com>
2023-09-19 22:41:45 +0200

    changes task 2 ?

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,34 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+const LOCAL_STORAGE_KEY = 'videoplayer-current-time';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const savedTime = localStorage.getItem(LOCAL_STORAGE_KEY);
+
+  if (savedTime) {
+    player.setCurrentTime(savedTime).catch(error => {
+      switch (error.name) {
+        case 'RangeError':
+          console.error(
+            'The time was less than 0 or greater than the video’s duration.'
+          );
+          break;
+
+        default:
+          console.error('Some unknown error occurred.');
+          break;
+      }
+    });
+  }
+});
+
+player.on(
+  'timeupdate',
+  throttle(data => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, data.seconds);
+  }, 1000)
+);
