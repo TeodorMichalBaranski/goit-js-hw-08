eba43553d1f8fb2592744c72140235f4406d8f5f
TeodorMichalBaranski <126625089+TeodorMichalBaranski@users.noreply.github.com>
2023-09-20 12:33:41 +0200

    changes

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -8,22 +8,6 @@ const LOCAL_STORAGE_KEY = 'videoplayer-current-time';
 
 document.addEventListener('DOMContentLoaded', () => {
   const savedTime = localStorage.getItem(LOCAL_STORAGE_KEY);
-
-  //   if (savedTime) {
-  //     player.setCurrentTime(savedTime).catch(error => {
-  //       switch (error.name) {
-  //         case 'RangeError':
-  //           console.error(
-  //             'The time was less than 0 or greater than the video’s duration.'
-  //           );
-  //           break;
-
-  //         default:
-  //           console.error('Some unknown error occurred.');
-  //           break;
-  //       }
-  //     });
-  //   }
 });
 
 player.on(
