62b784ac891e24094c2648a77d513c9faa641560
TeodorMichalBaranski <126625089+TeodorMichalBaranski@users.noreply.github.com>
2023-09-21 17:57:42 +0200

    changes task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -9,27 +9,26 @@ player.on(
   throttle(event => {
     localStorage.setItem('videoplayer-current-time', event.seconds.toString());
   }, 1000)
-); // Throttling the saving to once per second
+);
 
 window.addEventListener('DOMContentLoaded', () => {
   let savedTime = localStorage.getItem('videoplayer-current-time');
 
   if (savedTime) {
-    player
-      .setCurrentTime(parseFloat(savedTime))
-      .then(seconds => {
-        console.log('Video seeked to: ' + seconds);
-      })
-      .catch(error => {
-        switch (error.name) {
-          case 'RangeError':
-            console.error('Error: Time was out of range.');
-            break;
-          default:
-            console.error('Error: ', error);
-            break;
-        }
-      });
+    player.setCurrentTime(parseFloat(savedTime));
+    //   .then(seconds => {
+    //     console.log('Video seeked to: ' + seconds);
+    //   })
+    //   .catch(error => {
+    //     switch (error.name) {
+    //       case 'RangeError':
+    //         console.error('Error: Time was out of range.');
+    //         break;
+    //       default:
+    //         console.error('Error: ', error);
+    //         break;
+    //     }
+    //   });
   }
 });
 
